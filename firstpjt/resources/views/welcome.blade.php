a97c3845e1fa5721b0fc0ade117e848f57ebd09d
Thomas-DEV7 <thomas.felip16@gmail.com>
2023-02-15 16:41:00 -0300

    learn routes

---
+++ firstpjt/resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Page In Laravel</title>
+        <!-- Styles -->
+        <style>
+        </style>
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <h2>Pagina inicial</h2>
+        <a href="/contato">Contato</a>
+        <a href="login">Login</a>
+    </body>
+</html>
